3d8928f4387b9bb1e66056ca79bb5888903792b8
TriKriSta <Trikrista13@gmail.com>
2017-10-03 13:29:12 +0300

    add functions for use table countries in db

--- src/dex/db/dexdb.cpp
+++ src/dex/db/dexdb.cpp
@@ -1,5 +1,6 @@
 #include <ctime>
 #include "dex/dexdb.h"
+#include <iostream>
 
 namespace dex {
 
@@ -7,21 +8,60 @@ DexDB::DexDB(const boost::filesystem::path &path)
 {
     std::string dbFile = path.string() + "/dex.db";
     db = sqlite3pp::database(dbFile.c_str());
-    createTable();
-    addCurrentTime();
+    createTables();
 }
 
-void DexDB::addCurrentTime()
+void DexDB::addCountry(const std::string &iso, const std::string &name, const bool &enabled)
 {
-    sqlite3pp::command cmd(db, "INSERT INTO current (current_time) VALUES (?)");
-    int time = static_cast<int>(std::time(nullptr));
-    cmd.bind(1, time);
+    sqlite3pp::command cmd(db, "INSERT INTO countries (iso, name, enabled) VALUES (?, ?, ?)");
+    cmd.bind(1, iso, sqlite3pp::nocopy);
+    cmd.bind(2, name, sqlite3pp::nocopy);
+    cmd.bind(3, enabled);
     cmd.execute();
 }
 
-void DexDB::createTable()
+void dex::DexDB::editCountry(const std::string &iso, const bool &enabled)
 {
-    db.execute("create table if not exists current (current_time INT)");
+    sqlite3pp::command cmd(db, "UPDATE countries SET enabled = ? WHERE iso = ?");
+    cmd.bind(1, enabled);
+    cmd.bind(2, iso, sqlite3pp::nocopy);
+
+    cmd.execute();
+}
+
+void dex::DexDB::deleteCountry(const std::string &iso)
+{
+    sqlite3pp::command cmd(db, "DELETE FROM countries WHERE iso = ?");
+    cmd.bind(1, iso, sqlite3pp::nocopy);
+
+    cmd.execute();
+}
+
+std::map<std::string, CountryInfo> dex::DexDB::getCountriesInfo()
+{
+    std::map<std::string, CountryInfo> countries;
+
+    sqlite3pp::query qry(db, "SELECT iso, name, enabled FROM countries");
+
+    for (sqlite3pp::query::iterator i = qry.begin(); i != qry.end(); ++i) {
+        std::string iso;
+        std::string name;
+        bool enabled;
+        std::tie(iso, name, enabled) = (*i).get_columns<std::string, std::string, bool>(0, 1, 2);
+
+        CountryInfo info;
+        info.name = name;
+        info.enabled = enabled;
+
+        countries[iso] = info;
+    }
+
+    return countries;
+}
+
+void DexDB::createTables()
+{
+    db.execute("CREATE TABLE IF NOT EXISTS countries (iso VARCHAR(2) NOT NULL PRIMARY KEY, name VARCHAR(100), enabled BOOLEAN)");
 }
 
 }
--- src/dex/include/dex/dexdb.h
+++ src/dex/include/dex/dexdb.h
@@ -3,16 +3,26 @@
 
 #include "../../sqlite/sqlite3pp/sqlite3pp.h"
 #include <boost/filesystem/path.hpp>
+#include <map>
 
 namespace dex {
+
+struct CountryInfo {
+    std::string name;
+    bool enabled;
+};
 
 class DexDB {
 public:
     DexDB(const boost::filesystem::path &path);
 
-    void addCurrentTime();
+    void addCountry(const std::string &iso, const std::string &name, const bool &enabled = true);
+    void editCountry(const std::string &iso, const bool &enabled);
+    void deleteCountry(const std::string &iso);
+    std::map<std::string, CountryInfo> getCountriesInfo();
+
 private:
-    void createTable();
+    void createTables();
 
     sqlite3pp::database db;
 };
